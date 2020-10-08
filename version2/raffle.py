03efe1187cb2e89d867aa125063e17dc1fc55ebf
vagrant <vagrant@vagrant.vm>
2020-10-08 16:55:14 +0000

    progress on raffle functions

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,17 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+import Random
+
+def randomly_pick_winner(customers):
+    winner = random.choice(customers)
+    print(f'reach out to {name} at {email} and tell them they won!'.format(name = winner.name
+    email = winner.email))
+
+def run_raffle():
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+
+if __name__ == "__main__":
+    run_raffle()
